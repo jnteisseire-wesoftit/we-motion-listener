85f50e3561f2a3a4e5eded82dbfc18f41d99e0d1
jean-noel.teisseire <jean-noel.teisseire@wesoftit.com>
2022-02-22 12:04:39 +0100

    fixing installer

--- install-we-motion-listener.sh
+++ install-we-motion-listener.sh
@@ -4,12 +4,18 @@
 
 # Install git and clone we-motion-listener into the destination directory
 apt-get install git
-git clone https://github.com/jnteisseire-wesoftit/we-motion-listener
+git clone https://github.com/jnteisseire-wesoftit/we-motion-listener /etc/we-motion-listener
 chown -R motion.motion /etc/we-motion-listener
 chmod +x /etc/we-motion-listener/we-motion-listener.py
 
 
 # Create the log files and lock files and set ownership and permissions
-touch /var/log/we=motion-listener/we-motion-listener.log
+touch /var/log/we-motion-listener/we-motion-listener.log
 chown motion.motion /var/log/we-motion-listener/we-motion-listener.log
 chmod 664 /var/log/we-motion-listener/we-motion-listener.log
+
+# Change the File permissions
+sudo chown motion.motion /etc/we-motion-listener/we-motion-listener.py
+sudo chown motion.motion /etc/we-motion-listener/we-motion-listener.cfg
+sudo chmod 744 /etc/we-motion-listener/we-motion-listener.py
+sudo chmod 600 /etc/we-motion-listener/we-motion-listener.cfg
